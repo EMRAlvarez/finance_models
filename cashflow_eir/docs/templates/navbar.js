d39afde5c4816cbd42055d28faee849477e20294
James Briggs <james.briggs@hotmail.com>
2020-05-04 10:17:08 +0100

    added pages for docs

--- cashflow_eir/docs/templates/navbar.js
+++ cashflow_eir/docs/templates/navbar.js
@@ -2,18 +2,18 @@ document.write('\
 \
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">\
   <div class="container">\
-    <a class="navbar-brand" href="https://github.com/jamescalam">Finance Models</a>\
+    <a class="navbar-brand" href="https://github.com/jamescalam/finance_models/index.html">Finance Models</a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
       <span class="navbar-toggler-icon"></span>\
     </button>\
     <div class="collapse navbar-collapse" id="navbarResponsive">\
       <ul class="navbar-nav ml-auto">\
         <li class="nav-item">\
-          <a class="nav-link" href="readme.html">Main\
+          <a class="nav-link" href="https://github.com/jamescalam/finance_models/cashflow_eir/readme.html">Cashflow\
           </a>\
         </li>\
         <li class="nav-item">\
-          <a class="nav-link" href="https://medium.com/@jamescalam">Cashflow Modelling Article</a>\
+          <a class="nav-link" href="https://medium.com/@jamescalam">Step-by-step Cashflow Modelling</a>\
         </li>\
         <li class="nav-item">\
           <a class="nav-link" href="https://github.com/jamescalam/autodocs"><i>Autodocs</i></a>\
