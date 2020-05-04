ae485ff5233fe8806a1d1151c7451aaef866e8f9
James Briggs <james.briggs@hotmail.com>
2020-05-04 10:21:12 +0100

    link fix for pages index

--- cashflow_eir/docs/templates/navbar.js
+++ cashflow_eir/docs/templates/navbar.js
@@ -2,7 +2,7 @@ document.write('\
 \
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">\
   <div class="container">\
-    <a class="navbar-brand" href="http://jamescalam.github.io/finance_models/index.html">Finance Models</a>\
+    <a class="navbar-brand" href="http://jamescalam.github.io/finance_models">Finance Models</a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
       <span class="navbar-toggler-icon"></span>\
     </button>\
